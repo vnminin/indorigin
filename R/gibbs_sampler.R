7e411603813a7073c25194418f9f358e3761fde6
vnminin <vminin@uw.edu>
2015-07-24 12:06:36 -0700

    roxygenized runTwoStateGibbs

--- R/gibbs_sampler.R
+++ R/gibbs_sampler.R
@@ -1,3 +1,29 @@
+#' Gibbs sampler for a phylogenetic model with a binary trait.
+#'
+#' Runs a Gibbs sampler for a two-state CMTC 
+#' on a set of phylogenetic trees, with data 
+#' at the tips of the trees.
+#' 
+#' @param inputTrees \code{multiPhylo} object containing a list of trees.
+#' @param rootDist Numeric vector of size two with probabilities of the 
+#' two states at the root of the tree.
+#' @param traitData Vector of trait values, coded as 0 and 1.
+#' @param initLambda01 Initial value of the 0 -> 1 rate.
+#' @param initLambda10 Initial value of the 1 -> 0 rate.
+#' @param priorAlpha01 Shape parameter of the Gamma prior on the 0 -> 1 rate.
+#' @param priorBeta01 Rate parameter of the Gamma prior on the 0 -> 1 rate.
+#' @param priorAlpha10 Shape parameter of the Gamma prior on the 1 -> 0 rate.
+#' @param priorBeta10 Rate parameter of the Gamma prior on the 0 -> 1 rate.
+#' @param mcmcSize Total number of MCMC iterations. 
+#' @param mcmcBurnin Number of initial iterations to ignore.
+#' @param mcmcSubsample Integer controlling how many MCMC iterations to save. 
+#' For example, \code{mcmcSubsample = 10} saves every tenth iteration after ignoreing the first \code{mcmcBurnin} iterations.
+#'
+#' @return List with two elements: the input trees and a matrix of MCMC output.
+#' The columns of MCMC output matrix are Iteration number, Index of a tree 
+#' that was sampled at this iteration, Log posterior, 
+#' 0 -> 1 rate, 1 -> 0 rate, Number of 0 -> 1 jumps, Number of 1 -> 0 jumps,
+#' Time spent in state 0, Time spent in state 1
 runTwoStateGibbs <- function(inputTrees, rootDist, traitData, initLambda01, initLambda10, priorAlpha01, priorBeta01, priorAlpha10, priorBeta10, mcmcSize, mcmcBurnin, mcmcSubsample){
   
   ## Prepare trees and trait data  for Rcpp code
